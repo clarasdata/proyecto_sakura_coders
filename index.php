9d17903f84378e1b2b2aadfce28a36d058dafdb4
Valentina Toni <vale.toni93@gmail.com>
2024-03-21 14:12:42 +0100

    Primero commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" />
+    <title>Proyecto final Sakura Coders</title>
+</head>
+
+<body>
+
+    <header class="title-wrap">
+        <div class="title-content">
+            <h1>Sakura Coders</h1>
+        </div>
+    </header>
+
+    <button type="submit" class="button">Añadir miembro</button>
+
+    <main class="group-cards">
+
+        <div>
+            <img src="images/namine.jpg" alt="Imagen Namine">
+            <div class="text-card">
+                <h2>Naminé</h2>
+                <p>Nombre: Jessica</p>
+                <p>Género:</p>
+                <p>Edat:</p>
+                <p>Nacionalidad: Española</p>
+            </div>
+        </div>
+
+        <div>
+            <img src="images/namine.jpg" alt="Imagen Namine">
+            <div class="text-card">
+                <h2>Naminé</h2>
+                <p>Nombre: Jessica</p>
+                <p>Género:</p>
+                <p>Edat:</p>
+                <p>Nacionalidad:</p>
+            </div>
+        </div>
+
+        <div>
+            <img src="images/namine.jpg" alt="Imagen Namine">
+            <div class="text-card">
+                <h2>Naminé</h2>
+                <p>Nombre: Jessica</p>
+                <p>Género:</p>
+                <p>Edat:</p>
+                <p>Nacionalidad:</p>
+            </div>
+        </div>
+
+        <div>
+            <img src="images/namine.jpg" alt="Imagen Namine">
+            <div class="text-card">
+                <h2>Naminé</h2>
+                <p>Nombre: Jessica</p>
+                <p>Género:</p>
+                <p>Edat:</p>
+                <p>Nacionalidad:</p>
+            </div>
+        </div>
+
+        <div>
+            <img src="images/namine.jpg" alt="Imagen Namine">
+            <div class="text-card">
+                <h2>Naminé</h2>
+                <p>Nombre: Jessica</p>
+                <p>Género:</p>
+                <p>Edat:</p>
+                <p>Nacionalidad:</p>
+            </div>
+        </div>
+
+    </main>
+
+
+</body>
+
+</html>
